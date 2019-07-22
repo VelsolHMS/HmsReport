7193599a62d2be72e2bc64b1143ed8b10a281a51
GuduruUmesh <35020243+GuduruUmesh@users.noreply.github.com>
2019-07-22 18:17:21 +0530

    Initial commit

---
+++ HmsReport/Reports/CheckoutDataset.cs
@@ -0,0 +1,15 @@
+﻿namespace HmsReport.Reports
+{
+
+
+    partial class CheckoutDataset
+    {
+        partial class Datatable2DataTable
+        {
+        }
+
+        partial class BillDetailsDataTable
+        {
+        }
+    }
+}
